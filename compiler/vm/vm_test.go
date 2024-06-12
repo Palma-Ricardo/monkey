485659202a8aaf7ab4353e7ebe05e7329d78f5fc
Ricardo <ricardo.palma.rjp@gmail.com>
2024-06-12 08:57:10 -0500

    Added classic recursive fibonacci example to vm tests

--- compiler/vm/vm_test.go
+++ compiler/vm/vm_test.go
@@ -651,3 +651,27 @@ func TestRecursiveFunctions(tester *testing.T) {
 
 	runVmTests(tester, tests)
 }
+
+func TestRecursiveFibonacci(tester *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+            let fibonacci = fn(x) {
+                if (x == 0)  {
+                    return 0;
+                } else {
+                    if (x == 1) {
+                        return 1;
+                    } else {
+                        return fibonacci(x - 1) + fibonacci(x - 2);
+                    }
+                }
+            }
+            fibonacci(15);
+            `,
+			expected: 610,
+		},
+	}
+
+	runVmTests(tester, tests)
+}
